a0e36095d05532fe4454e265261ec7d68802e8d9
Yuki Ota <yukiotataitien@gmail.com>
2021-10-10 11:13:06 +0800

    新增env example參數 & 限制開發帳號登入

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
         try {
             
             if (env('PRODUCTION') === 'develop') {
-                if ($request->athlete['id'] !== env('STRAVA_DEV_ID')) {
+                if ($request->athlete['id'] !== env('STRAVA_DEV_ID', '93819542')) {
                     return response()->json(['status' => false, 'message' => '此網頁尚在開發階段，僅限開發帳號登入', 'data' => null], 404); 
                 }
             }
