1ccce927aa9e165e37cbd58d3774ffcab16badfc
BiBiOTA <yukiotataitien@gmail.com>
2021-11-13 17:23:24 +0800

    後台jwt 登入

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+    /**
+     * Get a JWT via given credentials.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+        if (!$token = Auth::guard()->attempt($credentials)) {
+            return response()->json(['status' => false, 'message' => '登入失敗'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+    /**
+     * Get a authenticated User.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(Auth::guard()->user());
+    }
+    /**
+     * Log the user out(Invalidate the Token).
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    
+    public function logout()
+    {
+        Auth::guard()->logout();
+        return response()->json(['status' => true, 'message'=>'登出成功']);
+    }
+    /**
+     * Refresh a Token.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::guard()->refresh());
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>Auth::guard()->factory()->getTTL()*60
+        ]);
+    }
+}
