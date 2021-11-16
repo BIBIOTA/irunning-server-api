4adb2943a0dda6e7d83778d8a1fd1f2574770b5b
BiBiOTA <yukiotataitien@gmail.com>
2021-11-16 17:27:46 +0800

    bug fixed -- 登入token問題修正 (Invalid argument supplied for foreach(), StravaActivitiesTrait.php:34)

--- app/Http/Controllers/Traits/StravaActivitiesTrait.php
+++ app/Http/Controllers/Traits/StravaActivitiesTrait.php
@@ -31,24 +31,26 @@ trait StravaActivitiesTrait
                     $formData = [
                         'user_id' => $token->user_id,
                     ];
-                    foreach($data as $key => $value) {
-                        if(in_array($key, $columns)) {
-                            if ($key === 'start_date_local') {
-                                $time_raw = strtotime($value);
-                                $time_mysql = Carbon::parse($time_raw);
-                                $formData[$key] = $time_mysql;
-                            } else {
-                                $formData[$key] = $value;
+                    if (is_array($data)) {
+                        foreach($data as $key => $value) {
+                            if(in_array($key, $columns)) {
+                                if ($key === 'start_date_local') {
+                                    $time_raw = strtotime($value);
+                                    $time_mysql = Carbon::parse($time_raw);
+                                    $formData[$key] = $time_mysql;
+                                } else {
+                                    $formData[$key] = $value;
+                                }
+                            }
+                            if ($key === 'map') {
+                                $formData['summary_polyline'] = $value['summary_polyline'];
                             }
                         }
-                        if ($key === 'map') {
-                            $formData['summary_polyline'] = $value['summary_polyline'];
+                        if ($data['type'] === 'Run') {
+                            app(Activity::class)->updateOrCreate([
+                                'id'=>$formData['id']
+                            ], $formData);
                         }
-                    }
-                    if ($data['type'] === 'Run') {
-                        app(Activity::class)->updateOrCreate([
-                            'id'=>$formData['id']
-                        ], $formData);
                     }
                 }
             } else {
