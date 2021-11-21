e094c7c74319f181b89a91dca02f00357fbda91a
BiBiOTA <yukiotataitien@gmail.com>
2021-11-21 21:22:13 +0800

    會員管理 - 會員檢視api & 跑步紀錄api | 前台取得跑步紀錄與後台共用方法

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 use App\Models\Member;
 use App\Models\Activity;
 
 use App\Http\Controllers\Traits\Running;
+use App\Http\Controllers\Traits\StravaActivitiesTrait;
 
 class MemberController extends Controller
 {
@@ -14,6 +19,7 @@ class MemberController extends Controller
     public $members;
 
     use Running;
+    use StravaActivitiesTrait;
 
     public function __construct()
     {
@@ -32,36 +38,104 @@ class MemberController extends Controller
 
             $data->getCollection()->transform(function($row){
 
-                $stat = $row->stat;
+                return $this->memberDataProcess($row);
                 
-                $activitiesMonth = $this->activity->getActivitiesMonth($row->id);
-        
-                $runningStatus = [
-                    'totalDistance' => 0,
-                    'monthDistance' => 0,
-                ];
-
-                if ($stat && isset($activitiesMonth)) {
-
-                    $runningStatus['totalDistance'] = floor($this->getDistance($stat->distance));
-                    $runningStatus['monthDistance'] = floor($this->getDistance($activitiesMonth));
-
-                }
-
-                return [
-                    'id' => $row->id,
-                    'username' => $row->username ?? '未填寫',
-                    'loginFrom' => $row->login_from,
-                    'totalDistance' => $runningStatus['totalDistance'],
-                    'monthDistance' => $runningStatus['monthDistance'],
-                    'runnerType' => $this->runnerType($row->runner_type),
-                    'lastLoginAt' => $row->memberToken->updated_at,
-                ];
             });
             
             return response()->json(['status' => true, 'message' => '取得資料成功', 'data' => $data], 200);
         }
 
         return response()->json(['status' => false, 'message' => '查無任何資料', 'data' => null], 404);
+    }
+
+    public function view (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'id'=>'required',
+        ], [
+            'id.required'=>'缺少uuid資料',
+        ]);
+        if ($validator->fails()){
+            return response()->json(['status'=>false, 'message'=>$validator->errors()->all()[0], 'data'=>null], 400);
+        }
+
+        $member = $this->members->find($request->id);
+
+        if ($member) {
+
+            $data['member'] = $this->memberDataProcess($member);
+
+            $this->filters = [
+                'id' => $request->id,
+            ];
+    
+            $activities = app(Activity::class)
+                        ->where('user_id', $request->id)
+                        ->orderBy('start_date_local', 'DESC')
+                        ->get();
+
+            if ($activities->count() > 0) {
+                $data['activities'] = $activities->map(function($row){
+                    return [
+                        'id' => $row->id,
+                        'distance' => $this->getDistanceIsFloor($row->distance),
+                        'movingTime' => $row->moving_time,
+                        'time' => $row->start_date_local,
+                    ];
+                });
+            }
+
+            return response()->json(['status' => true, 'message' => '取得資料成功', 'data' => $data], 200);
+
+        }
+
+        return response()->json(['status' => false, 'message' => '查無任何資料', 'data' => null], 404);
+
+    }
+
+    public function runningInfo (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'id'=>'required',
+            'user_id'=>'required',
+        ], [
+            'id.required'=>'缺少跑步紀錄uuid資料',
+            'user_id.required'=>'缺少會員uuid資料',
+        ]);
+        if ($validator->fails()){
+            return response()->json(['status'=>false, 'message'=>$validator->errors()->all()[0], 'data'=>null], 400);
+        }
+
+        return $this->getActivityFromStrava($request->user_id, $request->id);
+
+    }
+
+    private function memberDataProcess($row) {
+
+        $stat = $row->stat;
+        
+        $activitiesMonth = $this->activity->getActivitiesMonth($row->id);
+
+        $runningStatus = [
+            'totalDistance' => 0,
+            'monthDistance' => 0,
+        ];
+
+        if ($stat && isset($activitiesMonth)) {
+
+            $runningStatus['totalDistance'] = floor($this->getDistance($stat->distance));
+            $runningStatus['monthDistance'] = floor($this->getDistance($activitiesMonth));
+
+        }
+
+        return [
+            'id' => $row->id,
+            'username' => $row->username ?? '未填寫',
+            'loginFrom' => $row->login_from,
+            'totalDistance' => $runningStatus['totalDistance'],
+            'monthDistance' => $runningStatus['monthDistance'],
+            'runnerType' => $this->runnerType($row->runner_type),
+            'lastLoginAt' => $row->memberToken->updated_at,
+        ];
     }
 }
