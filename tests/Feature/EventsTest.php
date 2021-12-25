5f2c2b17a4693d04868092e33d40586d352aff18
BiBiOTA <yukiotataitien@gmail.com>
2021-12-25 12:02:04 +0800

    -- feat -- unit test -- Activities & EventsTest + pagination資料結構function和測試共有

--- tests/Feature/EventsTest.php
+++ tests/Feature/EventsTest.php
@@ -31,86 +31,50 @@ class EventsTest extends TestCase
         // 1: 全馬, 2: 半馬, 3: 三鐵
         $eventDistances = [1,2,3];
 
-        $dataStructure = [
-            'status',
-            'message',
-            'data' => [
-                'current_page',
-                'next_page_url',
-                'path',
-                'per_page',
-                'prev_page_url',
-                'to',
-                'total',
-                'data' => [
+        $dataStructure = $this->paginationStructure([
+            '*' => [
+                'id',
+                'link',
+                'event_status',
+                'event_name',
+                'event_certificate',
+                'event_date',
+                'event_time',
+                'location',
+                'agent',
+                'participate',
+                'created_at',
+                'updated_at',
+                'distance' => [
                     '*' => [
                         'id',
-                        'link',
-                        'event_status',
-                        'event_name',
-                        'event_certificate',
-                        'event_date',
-                        'event_time',
-                        'location',
-                        'agent',
-                        'participate',
+                        'event_id',
+                        'event_distance',
+                        'event_price',
+                        'event_limit',
                         'created_at',
                         'updated_at',
-                        'distance' => [
-                            '*' => [
-                                'id',
-                                'event_id',
-                                'event_distance',
-                                'event_price',
-                                'event_limit',
-                                'created_at',
-                                'updated_at',
-                            ],
-                        ],
                     ],
                 ],
-            ]
-        ];
+            ],
+        ]);
 
         // conditon
         for ($i = 0; $i <= count($eventDistances) - 1; $i++) {
             $eventDistance = $eventDistances[$i];
             for ($j = 0; $j <= count($dateRangeTest) - 1; $j++) {
                 $dateRange = $dateRangeTest[$j];
-                $pageCount = 1;
                 $data = [
                     'event_distance' => $eventDistance,
-                    'page' => $pageCount,
                     'startDay' => $dateRange['startDay'],
                     'endDay' => $dateRange['endDay'],
                 ];
-                for ($page = 1; $page <= $pageCount; $page++) {
-                    $response = $this->call('GET', 'api/events', $data);
-                    $response->assertStatus($dateRange['result']);
-                    if ($dateRange['result'] === 200) {
-                        $response->assertJsonStructure($dataStructure);
-                        if ($this->hasNextPage($response)) {
-                            $pageCount++;
-                        }
-                    } else {
-                        $response->assertJsonStructure($this->falseJsonStructure());
-                    }
-                }
+                $this->paginationTest('GET', 'api/activities', $data, $dataStructure);
             }
 
         } 
 
         // default
-        $pageCount = 1;
-        for ($page = 1; $page <= $pageCount; $page++) {
-            $response = $this->call('GET', 'api/events', [
-                'page' => $pageCount,
-            ]);
-            $response->assertStatus(200);
-            $response->assertJsonStructure($dataStructure);
-            if ($this->hasNextPage($response)) {
-                $pageCount++;
-            }
-        }
+        $this->paginationTest('GET', 'api/activities', [], $dataStructure);
     }
 }
