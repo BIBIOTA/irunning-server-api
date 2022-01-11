5dd068471f5992c0316227f2d176a1078e1d37b2
BiBiOTA <yukiotataitien@gmail.com>
2022-01-11 19:21:33 +0800

    -- feat -- 更新登入/取得活動紀錄feature test & 新增登出 feature test & 會員相關測試整合
    -- feat
    -- 更新登入/取得活動紀錄feature test & 新增登出 feature test & 會員相關測試整合

---
+++ tests/Feature/memberTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Member;
+use App\Models\MemberToken;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+use Carbon\Carbon;
+use Artisan;
+
+class MemberTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $stravaAthleteUrl = 'https://www.strava.com/api/v3/athlete';
+
+        $dataStructure = [
+            'status',
+            'message',
+            'data' => [
+                'access_token',
+                'token_type',
+                'expires_in',
+            ],
+        ];
+
+        $member = app(MemberToken::class)->where('member_id', env('API_MEMBER_ID'))->first();
+
+        $responseRefreashToken = $this->refreshStravaToken($member);
+
+
+        $responseAthlete = Http::withToken($member->access_token)->get($stravaAthleteUrl);
+
+        $responseAthlete = $responseAthlete->json();
+
+        $formData = array_merge($responseRefreashToken, ['athlete' => $responseAthlete]);
+
+        $response = $this->call('POST', 'api/login/login', $formData);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure($dataStructure);
+    }
+
+    public function testLogout()
+    {
+        $memberId = env('API_MEMBER_ID');
+
+        $member = app(Member::class)->where('id', env('API_MEMBER_ID'))->first();
+
+        $token = Auth::guard()->fromUser($member);
+
+        $response = $this->call('POST', 'api/login/logout', [], [], [], ['HTTP_Authorization' => 'Bearer ' . $token], []);
+
+        $response
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'status',
+            'message',
+        ]);
+    }
+
+    public function testActivities()
+    {
+        $dataStructure = $this->paginationStructure([
+            '*' => [
+                'id',
+                'name',
+                'pace',
+                'distance',
+                'moving_time',
+                'start_date_local',
+                'summary_polyline',
+            ],
+        ]);
+
+        $member = app(Member::class)->where('id', env('API_MEMBER_ID'))->first();
+        $token = Auth::guard()->fromUser($member);
+
+        $this->paginationTest('GET', 'api/activities', [], $dataStructure, ['HTTP_Authorization' => 'Bearer ' . $token]);
+    }
+}
