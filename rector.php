c7dc040a3bc05bd6ceb3515f2baa6ce021672042
BiBiOTA <yukiotataitien@gmail.com>
2022-05-16 22:26:29 +0800

    feat(rector): Update Rector

--- rector.php
+++ rector.php
@@ -1,27 +1,15 @@
 <?php
 
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Rector\Config\RectorConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-
-    $parameters = $containerConfigurator->parameters();
-
+return static function (RectorConfig $rectorConfig): void {
     // paths to refactor; solid alternative to CLI arguments
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $rectorConfig->paths([__DIR__ . '/app', __DIR__ . '/tests',  __DIR__ . '/routes', __DIR__ . '/database']);
 
     // is your PHP version different from the one you refactor to? [default: your PHP version], uses PHP_VERSION_ID format
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81);
-
-    // Path to PHPStan with extensions, that PHPStan in Rector uses to determine types
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan-for-config.neon');
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
 
     // Auto Import Names
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    // Remove Unused Imports
-    $services->set(NoUnusedImportsFixer::class);
+    $rectorConfig->importNames();
 };
