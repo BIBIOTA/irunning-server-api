4c6f417191c0ef778e105c8459e9d93ded104dd4
BiBiOTA <yukiotataitien@gmail.com>
2022-05-17 23:24:24 +0800

    feat(test): Update weather API Test

--- tests/Feature/WeatherTest.php
+++ tests/Feature/WeatherTest.php
@@ -3,53 +3,108 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Database\Seeders\WeatherSeeder;
 use Database\Seeders\WxDocumentSeeder;
+use App\Models\City;
+use App\Models\District;
+use App\Models\WeatherDocument;
+use App\Models\WeatherData;
+use App\Http\Controllers\Traits\WeatherTrait;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class WeatherTest extends TestCase
 {
     use RefreshDatabase;
+    use WeatherTrait;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $this->seed(WxDocumentSeeder::class);
-        $this->seed(WeatherSeeder::class);
     }
 
     public function testWeather()
     {
-        $cities = $this->getCityCountyData();
+        $city = app(City::class)::select('id', 'dataid')->where('city_name', '臺北市')->first();
 
-        
-        $distinct = $this->distinctDistricts();
+        $district = app(District::class)->where('district_name', '大安區')->first();
 
-        foreach ($cities as $city) {
-            $districts = $this->getDistrictsData($city->id);
-            foreach ($districts as $district) {
-                if (!in_array($district->district_name, $distinct)) {
-                    $response = $this->call('GET', 'api/weather', [
-                        'district_id' => $district->id,
-                    ]);
-                    $response->assertStatus(200);
-                    $response->assertJsonStructure([
-                        'status',
-                        'message',
-                        'data' => [
-                            'city',
-                            'district',
-                            'T',
-                            'AT',
-                            'PoP6h',
-                            'CI',
-                            'Wx',
-                            'WxValue',
-                            'updated_at',
-                        ]
-                    ]);
-                    break;
+        $this->createSingleWeatherData($city, $district);
+
+        $response = $this->call('GET', 'api/weather', [
+            'district_id' => $district->id,
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+            'data' => [
+                'city',
+                'district',
+                'T',
+                'AT',
+                'PoP6h',
+                'CI',
+                'Wx',
+                'WxValue',
+                'updated_at',
+            ]
+        ]);
+    }
+
+    private function createSingleWeatherData(City $city, District $district)
+    {
+        $resdatas = $this->getHttpWeatherData($city->dataid, $district->district_name);
+
+        if (count($resdatas) > 0) {
+            foreach ($resdatas['records']['locations'] as $data) {
+                $district->dataid = $data['dataid'];
+                $district->updated_at = Carbon::now();
+                $district->save();
+
+                foreach ($data['location'] as $location) {
+                    foreach ($location['weatherElement'] as $key => $weatherElement) {
+                        if ($this->getColumnsKey($weatherElement['elementName'])) {
+                            $weatherformData = [
+                                'id' => uniqid(),
+                                'name' => $weatherElement['elementName'],
+                                'description' => $weatherElement['description'],
+                            ];
+
+                            $weather = WeatherDocument::factory()->create($weatherformData);
+
+
+                            foreach ($weatherElement['time'] as $time) {
+                                if (isset($time['startTime']) && isset($time['endTime'])) {
+                                    $data = [
+                                        'id' => uniqid(),
+                                        'weather_document_id' => $weather->id,
+                                        'district_id' => $district->id,
+                                        'start_time' => $time['startTime'],
+                                        'end_time' => $time['endTime'],
+                                        'value' => $this->getValue($weatherElement['elementName'], $time['elementValue'])
+                                    ];
+
+                                    WeatherData::factory()->create($data);
+                                }
+                                if (isset($time['dataTime'])) {
+                                    $defaultEndTime = Carbon::parse($time['dataTime'])->addHours(3);
+
+                                    $data = [
+                                        'id' => uniqid(),
+                                        'weather_document_id' => $weather->id,
+                                        'district_id' => $district->id,
+                                        'start_time' => $time['dataTime'],
+                                        'end_time' => $defaultEndTime,
+                                        'value' => $this->getValue($weatherElement['elementName'], $time['elementValue'])
+                                    ];
+
+                                    WeatherData::factory()->create($data);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
