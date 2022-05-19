f19541bfe9dfb0e63d1abf76b9d84beda6b0ca31
BiBiOTA <yukiotataitien@gmail.com>
2022-05-20 00:08:07 +0800

    fix(redis): Fixed redis connection

--- config/database.php
+++ config/database.php
@@ -127,19 +127,17 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'redis'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'port' => 6379,
+            'database' => 0,
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'redis'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => 6379,
+            'database' => 1,
         ],
 
     ],
