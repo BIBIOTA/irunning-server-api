a5a47cac8d294edbafb2c33310fd8dd85a0b6ce0
BiBiOTA <yukiotataitien@gmail.com>
2022-05-25 00:15:43 +0800

    fix(cron): Fixed artisan DB command on production processing force

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,14 +22,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('DB:seed --class=WeatherSeeder')
+        $schedule->command('DB:seed --class=WeatherSeeder --force')
         ->hourly();
-        $schedule->command('DB:seed --class=AqiSeeder')->hourly();
+        $schedule->command('DB:seed --class=AqiSeeder --force')->hourly();
         $schedule->command('strava:refreashToken')->everyThreeHours();
         $schedule->command('strava:activities')->dailyAt('00:00');
         $schedule->command('weather:clear')->dailyAt('23:30');
         $schedule->command('events:send')->dailyAt('09:00');
-        $schedule->command('DB:seed --class=EventSeeder')
+        $schedule->command('DB:seed --class=EventSeeder --force')
         ->dailyAt('01:00');
     }
 
